c614c8ec42d538150cb29e3a8f04dc88b5e46e7a
Vaibhav Shejol <vaibhav.shejol@bnt-soft.com>
2026-01-29 15:36:52 +0530

    Implement '/ping' endpoint for application status
    Added a '/ping' endpoint to check application status.

--- src/main/java/com/example/DemoApiApplication.java
+++ src/main/java/com/example/DemoApiApplication.java
@@ -3,11 +3,17 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class DemoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApiApplication.class, args);
 	}
+
+    @GetMapping("/ping")
+    public String status() {
+        return "Application is running!";
+    }
 
 }
